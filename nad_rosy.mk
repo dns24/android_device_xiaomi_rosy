c799ae14b508ce2fd99158e950d78afbc8b32e9a
Wahid Nursidik <wahidns24@gmail.com>
2021-03-15 20:21:12 +0000

    Revert "rosy: remove nusantaraparts"
    This reverts commit bcf4b2bd12ad2507fb5256b82dfbbf665655ded4.

--- nad_rosy.mk
+++ nad_rosy.mk
@@ -23,6 +23,7 @@ $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Inherit some common Nusantara stuff.
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, packages/apps/NusantaraParts/nadproject.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rosy
